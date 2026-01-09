df428912c8b8db79c9add09541d65024bbf86e1b
Nagisa/Panda <49723922+PandaHexCode@users.noreply.github.com>
2026-01-09 17:05:21 +0100

    Add files via upload

---
+++ SuperPaperMarioPAL/SuperPaperMarioCheater.py
@@ -0,0 +1,44 @@
+import keyboard
+import dolphin_memory_engine
+import time
+
+dolphin_memory_engine.hook()
+
+key_state = {}
+
+def get_key_down(key):
+    global key_state
+
+    if key not in key_state:
+        key_state[key] = False
+
+    if keyboard.is_pressed(key) and not key_state[key]:
+        key_state[key] = True
+        return True
+    elif not keyboard.is_pressed(key) and key_state[key]:
+        key_state[key] = False
+    
+    return False
+
+start_time = time.time()
+delta_time = 0
+
+while True:
+    loop_start_time = time.time()
+    #Change position
+    
+    if keyboard.is_pressed('up'):
+        dolphin_memory_engine.write_float(0x805104B8,  dolphin_memory_engine.read_float(0x805104B8) + (855 * delta_time))
+    elif keyboard.is_pressed('down'):
+       dolphin_memory_engine.write_float(0x805104B8,  dolphin_memory_engine.read_float(0x805104B8) - (355 * delta_time))
+    
+    if keyboard.is_pressed('a'):
+        dolphin_memory_engine.write_float(0x805104B4,  dolphin_memory_engine.read_float(0x805104B4) - (155 * delta_time))
+    elif keyboard.is_pressed('d'):
+        dolphin_memory_engine.write_float(0x805104B4,  dolphin_memory_engine.read_float(0x805104B4) + (155 * delta_time))
+    
+    if keyboard.is_pressed('w'):
+        dolphin_memory_engine.write_float(0x805104BC,  dolphin_memory_engine.read_float(0x805104BC) - (155 * delta_time))    
+    elif keyboard.is_pressed('s'):
+        dolphin_memory_engine.write_float(0x805104BC,  dolphin_memory_engine.read_float(0x805104BC) + (155 * delta_time))       
+    delta_time = time.time() - loop_start_time    
